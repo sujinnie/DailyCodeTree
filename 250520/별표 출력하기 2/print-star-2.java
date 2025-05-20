96ff83b6a536990b20d51524e172efe6694a76c8
Sujin Jeong <sujin5262@naver.com>
2025-05-20 18:04:33 +0900

    Title: 별표 출력하기 2, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250520/별표 출력하기 2/print-star-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=n; i>0; i--) {
+            for(int j=i; j>0; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
