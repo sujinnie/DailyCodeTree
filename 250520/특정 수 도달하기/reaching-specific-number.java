64de5cc667f891d24eb89e31e4d75a64bf579baa
Sujin Jeong <sujin5262@naver.com>
2025-05-20 21:43:42 +0900

    Title: 특정 수 도달하기, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

--- 250520/특정 수 도달하기/reaching-specific-number.java
+++ 250520/특정 수 도달하기/reaching-specific-number.java
@@ -5,16 +5,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int[] arr = new int[10];
-        int sum = 0;
-        int cnt = 0;
         for (int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
-            if (arr[i] < 250) {
-                sum += arr[i];
-                cnt++;
-            }
         }
+        
+        int sum = 0;
+        int cnt = 0;
+        for (int num : arr) {
+            if (num >= 250) {
+                break;
+            }
+            sum += num;
+            cnt++;
+        }
+
         double avg = (double) sum / cnt;
+        
         System.out.printf("%d %.1f", sum, avg);
     }
 }
