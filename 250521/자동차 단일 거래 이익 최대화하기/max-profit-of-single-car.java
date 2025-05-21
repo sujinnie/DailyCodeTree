ae08e3945ecf50b842fc5b042af02df0a8e11ea4
Sujin Jeong <sujin5262@naver.com>
2025-05-21 23:59:16 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 118ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250521/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        if(n == 1) {
+            System.out.println(arr[0]);
+            return;
+        }
+
+        int minPrice = arr[0];
+        int maxPrice = 0;
+        int diff = 0;
+        for(int i=1; i<n; i++) {
+            if(minPrice < arr[i]) {
+                maxPrice = arr[i];
+                diff = maxPrice - minPrice;
+            }
+            else {
+                minPrice = arr[i];
+            }
+        }
+        System.out.println(diff);
+    }
+}
