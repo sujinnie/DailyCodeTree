3fee7bde941187663ba2a28268fe1edc33bdcfe2
Sujin Jeong <sujin5262@naver.com>
2025-05-21 16:33:34 +0900

    Title: 100 도달하기, Time: 3062ms, Memory: 38MB, Status: Time Limit Exceed - Codetree

---
+++ 250521/100 도달하기/reach-100.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(); 
+        int a = 1;
+        int b = N;
+
+        System.out.print(a + " " + b);
+
+        while (true) {
+            int c = a + b;        // 새로 만들 다음 항
+            System.out.print(" " + c);
+            
+
+            // 한 칸씩 앞으로 당기기
+            a = b;
+            b = c;
+        }
+    }
+}
