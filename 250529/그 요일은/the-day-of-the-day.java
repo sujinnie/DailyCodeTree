c3f78a26b74ef615d362fc6916c64b0528f0edd6
Sujin Jeong <sujin5262@naver.com>
2025-05-29 01:09:30 +0900

    Title: 그 요일은, Time: 136ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 250529/그 요일은/the-day-of-the-day.java
+++ 250529/그 요일은/the-day-of-the-day.java
@@ -12,25 +12,27 @@ public class Main {
         String[] day_of_week = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
         int[] num_of_days = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         
-        int mon = m1, day = d1, yoil = 0, cnt = 1;
+        int mon = m1, day = d1, yoil = 0;
+        int cnt = day_of_week[0].equals(A) ? 1 : 0;
+
         while(true) {
-            if(mon == m2 && day == d2) {
-                System.out.println(cnt);
-                break;
-            }
-
             yoil = (yoil+1) % 7;
-            if(day_of_week[yoil].equals(A)) {
-                cnt++;
-            }
-
             day++;
             if(day > num_of_days[mon]) {
                 day = 1;
                 mon++;
-                if (mon > 12) {
-                    mon = 1; 
-                }
+                // if (mon > 12) {
+                //     mon = 1; 
+                // }
+            }
+
+            if(day_of_week[yoil].equals(A)) {
+                cnt++;
+            }
+
+            if(mon == m2 && day == d2) {
+                System.out.println(cnt);
+                break;
             }
         }
     }
