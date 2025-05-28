ead1685a72c70e9e59dabc7c7d4a7ed58ed1dc5c
Sujin Jeong <sujin5262@naver.com>
2025-05-29 03:49:55 +0900

    Title: 왔다 갔던 구역 2, Time: 146ms, Memory: 10MB, Status: Passed - Codetree

--- 250529/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 250529/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,30 +1,66 @@
-import java.util.Scanner;
+// import java.util.Scanner;
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int N = sc.nextInt();
+//         int cur = 1000;
+//         int[] visited = new int[2000];
+//         for (int i = 0; i < N; i++) {
+//             int x = sc.nextInt();
+//             char dir = sc.next().charAt(0);
+//             if(dir == 'R') {
+//                 for(int j=cur; j < cur+x; j++) {
+//                     visited[j]++;
+//                 }
+//                 cur += x;
+//             }
+//             else {
+//                 for(int j=cur-1; j >= cur-x; j--) {
+//                     visited[j]++;
+//                 }
+//                 cur -= x;
+//             }
+//         }
+
+//         int cnt = 0;
+//         for(int i=0; i<2000; i++) {
+//             if(visited[i] >= 2) cnt++;
+//         }
+
+//         System.out.println(cnt);
+//     }
+// }
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int cur = 1000;
-        int[] visited = new int[2000];
+
+        Map<Integer, Integer> visited = new HashMap<>();
+        int cur = 0;
+
         for (int i = 0; i < N; i++) {
             int x = sc.nextInt();
             char dir = sc.next().charAt(0);
             if(dir == 'R') {
                 for(int j=cur; j < cur+x; j++) {
-                    visited[j]++;
+                    visited.put(j, visited.getOrDefault(j, 0) + 1);
                 }
                 cur += x;
             }
             else {
                 for(int j=cur-1; j >= cur-x; j--) {
-                    visited[j]++;
+                    visited.put(j, visited.getOrDefault(j, 0) + 1);
                 }
                 cur -= x;
             }
         }
 
         int cnt = 0;
-        for(int i=0; i<2000; i++) {
-            if(visited[i] >= 2) cnt++;
+        for(Integer val : visited.values()) {
+            if(val >= 2) cnt++;
         }
 
         System.out.println(cnt);
