24d63dcc1a4639dd0fd1472c8b27082b8bc5b4ae
Sujin Jeong <sujin5262@naver.com>
2025-05-29 01:01:53 +0900

    Title: 그 요일은, Time: 112ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250529/그 요일은/the-day-of-the-day.java
+++ 250529/그 요일은/the-day-of-the-day.java
@@ -10,7 +10,7 @@ public class Main {
         String A = sc.next();
         
         String[] day_of_week = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int[] num_of_days = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         
         int mon = m1, day = d1, yoil = 0, cnt = 1;
         while(true) {
